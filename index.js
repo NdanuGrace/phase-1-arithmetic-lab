469dad331b730467d4ba24353ece72883a37d540
gracekyalo <gracendanu27@gmail.com>
2022-08-17 20:21:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// equation with multiplied results equal 62
+const num1=2; 
+const num2=31;
+const multiply= num1 * num2;
 
-// Write your code here
+
+// random int greater than 0
+const random= Math.floor(Math.random() * 10) + 1;
+
+
+
+// equation with a remainder equal to 4
+const num3= 22;
+const num4= 6;
+const mod= num3 % num4;
+
+
+
+// returns 20 as highest number
+
+ const max = Math.max (4,5,11,12,20,15)
+
